c27ae91490ce719b6822afc554b6cf96914fc442
user76335 <11958@holbertonstudents.com>
2026-02-18 21:20:09 +0300

    Create 5-sum_list.py

---
+++ 5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""5. Complex types - list of floats"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """return sums(input_list)"""
+    return sum(input_list)
