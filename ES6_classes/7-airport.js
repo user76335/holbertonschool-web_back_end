01ad089186d0ab7465865a1e4dbc3b552a2453f1
user76335 <11958@holbertonstudents.com>
2026-03-15 02:57:05 +0300

    Create 7-airport.js

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,22 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
